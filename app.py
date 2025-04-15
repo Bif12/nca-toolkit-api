41d771ddc8d60a8348159c4358a24c7e13c444ba
bif12 <khalid.bifounra.98@edu.uiz.ac.ma>
2025-04-15 19:54:49 +0000

    fix: exposed app on port 8000 for Railway

--- app.py
+++ app.py
@@ -189,5 +189,6 @@ def create_app():
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
+
